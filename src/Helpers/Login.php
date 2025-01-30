e00412209495eadf29364222f70b54a71e2379fd
Michael Drennen <michaeldrennen74@gmail.com>
2025-01-29 19:39:26 -0700

    Login works.

--- src/Helpers/Login.php
+++ src/Helpers/Login.php
@@ -3,20 +3,18 @@
 namespace DPRMC\RemitSpiderDeutscheBank\Helpers;
 
 
-use DPRMC\RemitSpiderDeutscheBank\Exceptions\ExceptionLoginIncorrect;
 use DPRMC\RemitSpiderDeutscheBank\RemitSpiderDeutscheBank;
 use GuzzleHttp\Client;
-use HeadlessChromium\Clip;
 use HeadlessChromium\Cookies\CookiesCollection;
 use HeadlessChromium\Page;
-use Psr\Http\Message\ResponseInterface;
+
 
 /**
  *
  */
 class Login {
 
-    const URL_LOGIN  = 'https://tss.sfs.db.com/search';
+    const URL_LOGIN  = 'https://tss.sfs.db.com';
     const URL_LOGOUT = 'https://identity.db.com/auth/realms/global/protocol/openid-connect/logout';
 
     const LOGIN_LINK_BUTTON_Y = 140;
@@ -60,13 +58,15 @@ class Login {
     /**
      * @param string $user
      * @param string $pass
+     *
      * @return string
-     * @throws ExceptionLoginIncorrect
      * @throws \HeadlessChromium\Exception\CommunicationException
      * @throws \HeadlessChromium\Exception\CommunicationException\CannotReadResponse
      * @throws \HeadlessChromium\Exception\CommunicationException\InvalidResponse
      * @throws \HeadlessChromium\Exception\CommunicationException\ResponseHasError
+     * @throws \HeadlessChromium\Exception\ElementNotFoundException
      * @throws \HeadlessChromium\Exception\FilesystemException
+     * @throws \HeadlessChromium\Exception\JavascriptException
      * @throws \HeadlessChromium\Exception\NavigationExpired
      * @throws \HeadlessChromium\Exception\NoResponseAvailable
      * @throws \HeadlessChromium\Exception\OperationTimedOut
@@ -75,81 +75,33 @@ class Login {
     public function login( string $user, string $pass ): string {
         $this->Debug->_debug( "Navigating to login screen at: " . self::URL_LOGIN );
         $this->Page->navigate( self::URL_LOGIN )->waitForNavigation( Page::NETWORK_IDLE );
-        sleep( 5 );
+        //sleep( 5 );
 
-        $this->_requestAnonymousToken();
-        $this->Debug->_debug( "This is the Bearer Token retrieved from _requestAnonymousToken(): " . $this->bearerToken );
+        $this->Debug->_screenshot( "1_home_page" );
+        $this->Debug->_html( "1_home_page" );
 
-
-        $this->Debug->_screenshot( 'start_page' );
-        $this->Debug->_html( 'start_page' );
-
-        $this->Debug->_debug( "Deleting the overlay" );
-        $this->Page->evaluate( "document.querySelector('.jss338').remove();" );
-
-        $this->Debug->_screenshot( 'page_without_overlay' );
-        $this->Debug->_html( 'page_without_overlay' );
-
-        $this->Debug->_debug( "Clicking Login button." );
-
-        $this->Debug->_screenshot( 'where_i_clicked_to_login', new Clip( 0, 0, self::LOGIN_LINK_BUTTON_X, self::LOGIN_LINK_BUTTON_Y ) );
-
-        $this->Page->mouse()
-                   ->move( self::LOGIN_LINK_BUTTON_X, self::LOGIN_LINK_BUTTON_Y )
-                   ->click();
+        $this->Page->mouse()->find('button.jss98')->click();
 
         $this->Page->waitForReload();
 
-        $this->Debug->_screenshot( 'login_page' );
-        $this->Debug->_html( 'login_page' );
+        $this->Debug->_screenshot( "2_login_page" );
+        $this->Debug->_html( "2_login_page" );
 
-        $this->Debug->_debug( "Should be on login button." );
-
-        $this->Debug->_debug( "Filling out user and pass." );
         $this->Page->evaluate( "document.querySelector('#username').value = '" . $user . "';" );
         $this->Page->evaluate( "document.querySelector('#password').value = '" . $pass . "';" );
 
-        $this->Debug->_screenshot( 'filled_in_user_pass' );
-        $this->Debug->_html( 'filled_in_user_pass' );
-        $this->Debug->_screenshot( 'the_login_button', new Clip( 0, 0, self::LOGIN_BUTTON_X, self::LOGIN_BUTTON_Y ) );
+        $this->Debug->_screenshot( "3_filled_in_login_page" );
+        $this->Debug->_html( "3_filled_in_login_page" );
 
-        $this->Debug->_debug( "Clicking the login button." );
-        $this->Page->mouse()
-                   ->move( self::LOGIN_BUTTON_X, self::LOGIN_BUTTON_Y )
-                   ->click();
+        $this->Page->mouse()->find('#kc-login')->click();
         $this->Page->waitForReload();
-        sleep( 5 );
 
+        sleep(5);
 
-        $this->Debug->_screenshot( 'am_i_logged_in' );
-        $this->Debug->_html( 'am_i_logged_in' );
+        $this->Debug->_screenshot( "4_am_i_logged_in" );
+        $this->Debug->_html( "4_am_i_logged_in" );
 
-        $currentUrl = $this->Page->getCurrentUrl();
-        $this->Debug->_debug( "Currently at: " . $currentUrl );
-
-        $postLoginHTML = $this->Page->getHtml();
-
-        if ( str_contains( $postLoginHTML, 'Please check your entries' ) ):
-            throw new  ExceptionLoginIncorrect( "Login appears to be incorrect. Check for a changed password." );
-        endif;
-
-        $this->Debug->_debug( "I appear to be logged in." );
-
-        $this->Debug->_debug( "About to spit out all the cookies." );
-
-        $this->Debug->_debug( "Requesting config route: " . $currentUrl );
-        $this->_requestConfig();
-        dump( $this->config );
-
-        $this->Debug->_debug( "Requesting token route: " . $currentUrl );
-        $this->_requestToken();
-        dump( $this->token );
-
-        $this->Debug->_debug( "Requesting claims route: " . $currentUrl );
-        $this->_requestClaims();
-        dump($this->claims);
-
-        return $postLoginHTML;
+        return $this->Page->getHtml();
     }
 
 
